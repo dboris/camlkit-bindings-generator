34fa2290674354e9bc52c6dbd2671b234261cc4c
Boris D. <boris.dob@gmail.com>
2024-08-02 17:03:18 -0700

    Add option to load framework bundle.

--- inspect/main.ml
+++ inspect/main.ml
@@ -1,3 +1,4 @@
+open Foundation
 open Runtime
 
 let usage = {|
@@ -7,18 +8,30 @@ Usage: inspect-rt -libs | -classes <lib-name> | -methods <class-name>
 let show_libs = ref false
 let show_classes = ref ""
 let show_methods = ref ""
+let load_fw = ref ""
 
 let speclist =
   [ ("-libs", Arg.Set show_libs, "Show loaded frameworks and libraries.")
   ; ("-classes", Arg.Set_string show_classes, "Show classes in <lib>")
   ; ("-methods", Arg.Set_string show_methods, "Show methods in <class>")
+  ; ("-load", Arg.Set_string load_fw, "Load framework bundle <fw-path>")
   ]
+
+let load_framework () =
+  if not (String.equal !load_fw "") then
+    let bundle =
+      NSBundle._class_ |> NSBundle.C.bundleWithPath (new_string !load_fw) in
+    if is_nil bundle then
+      Printf.eprintf "Framework bundle not found!\n%!"
+    else
+      NSBundle.load bundle |> ignore
 
 let () =
   Arg.parse speclist ignore usage;
   let lib = !show_classes
   and cls = !show_methods
   in
+  load_framework ();
   if !show_libs then
     List.iter print_endline (Inspect.loaded_library_names ())
   else if not (String.equal lib "") then
